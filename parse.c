d244ee6da469c5ecea9a9517884bf1bd4f9d9624
Zack Sheldon <zsheldon@oberlin.edu>
2015-10-31 20:19:50 -0400

    finished parse.c the test file for parsing input

--- parse.c
+++ parse.c
@@ -1,44 +1,48 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-
-int alingment = 0;
+int alignment = 0;
 int compress = 0;
 int width = 72;
-
-main (int argc, char *argv[]){
+char *ptr;
+int main (int argc, char *argv[]){
 int i =1;
 while (i < argc){
-	printf("%s", argv[i]);
+	//printf("argument is: %s\ni=%d", argv[i],i);
 	if (!strcmp(argv[i] , "-w")){
 	    //width flag
 	    i++;
-	    if isdigit(argv[i]){
-		width = argv[i];
-		i++;
+	    //printf("width start\n");
+	    width = atoi(argv[i]);
+	    i++;
+	    //printf("width end - %d", width);
 	    }
-	}
+	
 	else if (!strcmp(argv[i] , "-r")){
 	    //right alignment
 	    alignment = 1;
 	    i++;
+	    //printf("right\nalignment=%d", alignment);
 	}
 	else if (!strcmp(argv[i] , "-j")){
 	    //justified
 	    alignment = 2;
 	    i++;
+	    //printf("justified\nalignment=%d", alignment);
 	}
 	else if (!strcmp(argv[i] , "-s")){
 	    //compress spaces
 	    compress = 1;
 	    i++;
+	    //printf("compress");
 	}
 	else if ((!strcmp(argv[i] , "-h")) || (!strcmp(argv[i] , "-?"))){
+	    printf("Usage message**");
+	    return 0;
 	    //print usage message and exit program
 	}
-	else {
-	    //exit program. Case of no additional arguments is handled by while loop
-	}
+	
     }
 }
