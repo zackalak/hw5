5b4ae4b93190b023e394aa8431d57d1c9e765c4e
zackalak <zsheldon@oberlin.edu>
2015-10-31 21:41:35 -0400

    Added in command line parsing

--- inputtest2.c
+++ inputtest2.c
@@ -39,7 +39,9 @@ struct wordnode {
     
     //pointer to the next word
     struct wordnode *nextword;
-    
+    int alignment = 0;
+    int compress = 0;
+    int width = 72;
     int length;
 };
 
@@ -98,6 +100,44 @@ int main (int argc, char *argv[]) {
     struct wordnode *input = &rootnode;
     //pointer to last node
     struct wordnode *tail;
+    
+    int i =1;
+while (i < argc){
+	//printf("argument is: %s\ni=%d", argv[i],i);
+	if (!strcmp(argv[i] , "-w")){
+	    //width flag
+	    i++;
+	    //printf("width start\n");
+	    width = atoi(argv[i]);
+	    i++;
+	    //printf("width end - %d", width);
+	    }
+	
+	else if (!strcmp(argv[i] , "-r")){
+	    //right alignment
+	    alignment = 1;
+	    i++;
+	    //printf("right\nalignment=%d", alignment);
+	}
+	else if (!strcmp(argv[i] , "-j")){
+	    //justified
+	    alignment = 2;
+	    i++;
+	    //printf("justified\nalignment=%d", alignment);
+	}
+	else if (!strcmp(argv[i] , "-s")){
+	    //compress spaces
+	    compress = 1;
+	    i++;
+	    //printf("compress");
+	}
+	else if ((!strcmp(argv[i] , "-h")) || (!strcmp(argv[i] , "-?"))){
+	    printf("Usage message**");
+	    return 0;
+	    //print usage message and exit program
+	}
+	
+    }
 
 
     //char blanks[ 100 ] = "";
